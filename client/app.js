7cbe8db7012f610d8f75dbadb60b9d28ebc7e1ac
wmaqingbo <wmaqingbo@163.com>
2017-12-26 17:02:27 +0800

    热映中

--- client/app.js
+++ client/app.js
@@ -3,7 +3,13 @@ var qcloud = require('./vendor/wafer2-client-sdk/index')
 var config = require('./config')
 
 App({
-    onLaunch: function () {
-        qcloud.setLoginUrl(config.service.loginUrl)
-    }
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  }
 })
